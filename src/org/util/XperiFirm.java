ddf0282c6e903e4c1dfb4305d29337c995915bf4
Androxyde <s1flashtool@gmail.com>
2015-05-31 22:20:56 +0200

    Check xperifirm errors on launch + chmod 755 on xperifirm wrapper for macOS

--- src/org/util/XperiFirm.java
+++ src/org/util/XperiFirm.java
@@ -35,53 +35,61 @@ public class XperiFirm {
 	private static Logger logger = Logger.getLogger(XperiFirm.class);
 
 	public static void run() throws Exception {
-		String version = IOUtils.toString(new URL("http://www.iagucool.com/xperifirm/version"));
-		String downloadurl = IOUtils.toString(new URL("http://www.iagucool.com/xperifirm/download"));
-		TextFile tf = new TextFile(OS.getFolderUserFlashtool()+File.separator+"XperiFirm.version","ISO8859-15");
 		try {
-			tf.readLines();
-			if (!version.equals(tf.getLines().iterator().next())) {
+			String version = IOUtils.toString(new URL("http://www.iagucool.com/xperifirm/version"));
+			String downloadurl = IOUtils.toString(new URL("http://www.iagucool.com/xperifirm/download"));
+			TextFile tf = new TextFile(OS.getFolderUserFlashtool()+File.separator+"XperiFirm.version","ISO8859-15");
+			try {
+				tf.readLines();
+				if (!version.equals(tf.getLines().iterator().next())) {
+					tf.open(false);
+					logger.info("Downloading latest XperiFirm");
+					OS.unpackArchive(new URL(downloadurl), new File(OS.getFolderUserFlashtool()));
+					tf.write(version);
+					tf.close();
+				}
+			} catch (FileNotFoundException fne) {
 				tf.open(false);
 				logger.info("Downloading latest XperiFirm");
-				OS.unpackArchive(new URL(downloadurl), new File(OS.getFolderUserFlashtool()));
+				OS.unpackArchive(new URL(downloadurl), new File(OS.getFolderUserFlashtool()));			tf.write(version);
 				tf.write(version);
 				tf.close();
 			}
-		} catch (FileNotFoundException fne) {
-			tf.open(false);
-			logger.info("Downloading latest XperiFirm");
-			OS.unpackArchive(new URL(downloadurl), new File(OS.getFolderUserFlashtool()));			tf.write(version);
-			tf.write(version);
-			tf.close();
-		}
-		ProcessBuilderWrapper command;
-		List<String> cmdargs = new ArrayList<String>();
-		if (OS.getName().equals("windows")) {
-			cmdargs.add(OS.getPathXperiFirm());
-			cmdargs.add("-o");
-			cmdargs.add("\""+OS.getFolderFirmwaresDownloaded()+"\"");
-		}
-		else {
-			cmdargs.add(OS.getPathXperiFirmWrapper());
-			cmdargs.add(OS.getPathXperiFirm());
-			cmdargs.add(OS.getFolderFirmwaresDownloaded());
-		}
-		command = new ProcessBuilderWrapper(cmdargs);
-		
-		String[] downloaded = new File(OS.getFolderFirmwaresDownloaded()).list();
-		for (int i = 0; i<downloaded.length;i++) {
-			File bundled = new File(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i]+File.separator+"bundled");
-			if (bundled.exists()) continue;
-			File fwinfo = new File(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i]+File.separator+"fwinfo.xml");
-			if (fwinfo.exists()) {
-				XMLFwInfo info = new XMLFwInfo(fwinfo);
-				logger.info("Creating bundle for "+info.getProduct()+" "+info.getOperator()+" "+info.getVersion());
-				try {
-					createBundle(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i],info);
-				} catch (Exception e) {
-					logger.error(e.getMessage());
+			ProcessBuilderWrapper command=null;
+			try {
+				List<String> cmdargs = new ArrayList<String>();
+				if (OS.getName().equals("windows")) {
+					cmdargs.add(OS.getPathXperiFirm());
+					cmdargs.add("-o");
+					cmdargs.add("\""+OS.getFolderFirmwaresDownloaded()+"\"");
+				}
+				else {
+					cmdargs.add(OS.getPathXperiFirmWrapper());
+					cmdargs.add(OS.getPathXperiFirm());
+					cmdargs.add(OS.getFolderFirmwaresDownloaded());
+				}
+				command = new ProcessBuilderWrapper(cmdargs);
+			}
+			catch (Exception e) {
+				throw new Exception(command.getStdOut()+" / "+command.getStdErr());
+			}
+			String[] downloaded = new File(OS.getFolderFirmwaresDownloaded()).list();
+			for (int i = 0; i<downloaded.length;i++) {
+				File bundled = new File(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i]+File.separator+"bundled");
+				if (bundled.exists()) continue;
+				File fwinfo = new File(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i]+File.separator+"fwinfo.xml");
+				if (fwinfo.exists()) {
+					XMLFwInfo info = new XMLFwInfo(fwinfo);
+					logger.info("Creating bundle for "+info.getProduct()+" "+info.getOperator()+" "+info.getVersion());
+					try {
+						createBundle(OS.getFolderFirmwaresDownloaded()+File.separator+downloaded[i],info);
+					} catch (Exception e) {
+						logger.error(e.getMessage());
+					}
 				}
 			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
 		}
 	}
 
