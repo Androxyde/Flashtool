0266c87300b8b24b476e219dfc8d19598ff2439f
Androxyde <s1flashtool@gmail.com>
2017-01-03 21:52:38 +0100

    Search for version only fsc for a device family

--- src/flashsystem/X10flash.java
+++ src/flashsystem/X10flash.java
@@ -615,7 +615,7 @@ public class X10flash {
     	} catch (Exception e) {
     	}
     	DeviceEntry dev = Devices.getDeviceFromVariant(getCurrentDevice());
-    	return dev.getFlashScript(getCurrentDevice(),_bundle.getVersion());
+    	return dev.getFlashScript(_bundle.getVersion());
     }
   
     public void runScript() {
--- src/org/system/DeviceEntry.java
+++ src/org/system/DeviceEntry.java
@@ -359,25 +359,13 @@ public class DeviceEntry {
     	return (official || custom || genericofficial || genericcustom || defaultofficial || defaultcustom);
     }
 
-    public String getFlashScript(String model, String version) {
+    public String getFlashScript(String version) {
 
-    	boolean official = new File(getDeviceDir()   + File.separator+model+"_"+version+".fsc").exists();
-    	boolean custom   = new File(getMyDeviceDir() + File.separator+model+"_"+version+".fsc").exists();
-    	
-    	boolean genericofficial = new File(getDeviceDir()   + File.separator+model+".fsc").exists();
-    	boolean genericcustom   = new File(getMyDeviceDir() + File.separator+model+".fsc").exists();
-    	
-    	boolean defaultofficial = new File(getDeviceDir()   + File.separator+"default.fsc").exists();
-    	boolean defaultcustom   = new File(getMyDeviceDir() + File.separator+"default.fsc").exists();
+    	boolean official = new File(getDeviceDir()   + File.separator+version+".fsc").exists();
+    	boolean custom   = new File(getMyDeviceDir() + File.separator+version+".fsc").exists();
 
-    	if (custom)   return getMyDeviceDir()+File.separator+model+"_"+version+".fsc";
-    	if (official) return getDeviceDir()+File.separator+model+"_"+version+".fsc";
-    	
-    	if (genericcustom)   return getMyDeviceDir()+File.separator+model+".fsc";
-    	if (genericofficial) return getDeviceDir()+File.separator+model+".fsc";
-    	
-    	if (defaultcustom)   return getMyDeviceDir()+File.separator+"default.fsc";
-    	if (defaultofficial) return getDeviceDir()+File.separator+"default.fsc";
+    	if (custom)   return getMyDeviceDir()+File.separator+version+".fsc";
+    	if (official) return getDeviceDir()+File.separator+version+".fsc";
 
     	return "";
     }
